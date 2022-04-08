b905be21149a3bccba1f19a086efc0d53f6d2c3a
chadrak <chadrak@outlook.com>
2022-04-08 02:51:03 +0100

    Expense Tracker - Functionality

--- src/App.js
+++ src/App.js
@@ -1,38 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
-const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Gym Membership',
-      amount: 17.99,
-      date: new Date(2021, 9, 14),
-    },
-    {
-      id: 'e2',
-      title: 'Monitor',
-      amount: 120,
-      date: new Date(2020, 7, 25)
-    },
-    {
-      id: 'e3',
-      title: 'Monitor Arm',
-      amount: 35,
-      date: new Date(2021, 11, 24),
-    },
-    {
-      id: 'e4',
-      title: 'Wilson EVO NXT',
-      amount: 79.95,
-      date: new Date(2022, 1, 3),
-    },
-  ];
+// initialising state with dummy expenses
+const initialExpenses = [
+  {
+    id: 'e1',
+    title: 'Gym Membership',
+    amount: 17.99,
+    date: new Date(2021, 9, 14),
+  },
+  {
+    id: 'e2',
+    title: 'Monitor',
+    amount: 120,
+    date: new Date(2021, 7, 25)
+  },
+  {
+    id: 'e3',
+    title: 'Monitor Arm',
+    amount: 35,
+    date: new Date(2021, 11, 24),
+  },
+  {
+    id: 'e4',
+    title: 'Wilson EVO NXT',
+    amount: 79.95,
+    date: new Date(2022, 1, 3),
+  },
+  {
+    id: 'e5',
+    title: 'Mechanical Keyboard',
+    amount: 89.99,
+    date: new Date(2022, 4, 22),
+  },
+];
 
+const App = () => {
+  const [expenses, setExpenses] = useState(initialExpenses);
+
+  // adds newly input expense to the previous existing expenses
   const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
 
   return (
