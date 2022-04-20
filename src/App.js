6300ab9e2fa4a889a521520e2e34e61b432e6d51
Chadrak Holondo <89146273+chadrakh@users.noreply.github.com>
2022-04-20 16:15:52 +0100

    To-Do List
    to-do list starting project

--- src/App.js
+++ src/App.js
@@ -1,65 +1,52 @@
 import React, { useState } from 'react';
-import Expenses from './components/Expenses/Expenses';
-import NewExpense from './components/NewExpense/NewExpense';
 
-// initialising state with dummy expenses
-const initialExpenses = [
-  {
-    id: 'e1',
-    title: 'Gym Membership',
-    amount: 17.99,
-    date: new Date(2021, 9, 14),
-  },
-  {
-    id: 'e2',
-    title: 'Monitor',
-    amount: 120,
-    date: new Date(2021, 7, 25)
-  },
-  {
-    id: 'e3',
-    title: 'Monitor Arm',
-    amount: 35,
-    date: new Date(2021, 11, 24),
-  },
-  {
-    id: 'e4',
-    title: 'Wilson EVO NXT',
-    amount: 79.95,
-    date: new Date(2022, 1, 3),
-  },
-  {
-    id: 'e5',
-    title: 'Mechanical Keyboard',
-    amount: 89.99,
-    date: new Date(2022, 4, 22),
-  },
-];
+import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
+import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
+import './App.css';
 
 const App = () => {
-  const [expenses, setExpenses] = useState(initialExpenses);
+  const [courseGoals, setCourseGoals] = useState([
+    { text: 'Do all exercises!', id: 'g1' },
+    { text: 'Finish the course!', id: 'g2' }
+  ]);
 
-  // adds newly input expense to the previous existing expenses
-  const addExpenseHandler = expense => {
-    setExpenses((prevExpenses) => {
-      return [expense, ...prevExpenses];
+  const addGoalHandler = enteredText => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: 'goal1' });
+      return updatedGoals;
     });
   };
 
-  return (
-    <>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses}/>
-    </>
+  const deleteItemHandler = goalId => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+      return updatedGoals;
+    });
+  };
+
+  let content = (
+    <p style={{ textAlign: 'center' }}>No goals... Maybe add one?</p>
   );
 
-  /* return React.createElement(
-     'div',
-     {},
-     React.createElement('h2', {}, 'Expense Tracker'),
-     React.createElement(Expenses, {items: expenses}),    
-     ); */
+  if (courseGoals.length > 0) {
+    content = (
+      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
+    );
+  }
 
-}
+  return (
+
+      <div>
+        <section id="goal-form">
+          <CourseInput onAddGoal={addGoalHandler} />
+        </section>
+        <section id="goals">
+          {content}
+        </section>
+      </div>
+
+  );
+};
 
 export default App;
